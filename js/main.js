7bca513d7c0a29a5cf15f8821f021a1b92a44b62
Yaroslav <yaraq64@gmail.com>
2018-08-11 22:19:53 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -7,11 +7,25 @@
 
 $(document).ready(function() {
   frontSlider();
+  roomsSlider();
 });
 
 
 function frontSlider() {
   let $slider = $('.front-slider');
+  if ($slider.length) {
+    $slider.flickity({
+      dragThreshold: 20,
+      cellAlign: 'left',
+      pageDots: true,
+      wrapAround: true,
+      imagesLoaded: true
+    });
+  }
+}
+
+function roomsSlider() {
+  let $slider = $('.rooms-slider');
   if ($slider.length) {
     $slider.flickity({
       dragThreshold: 20,
